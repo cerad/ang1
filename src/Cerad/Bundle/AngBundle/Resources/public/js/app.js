1e9833c001667fa6d8b24cdf896ec2c17b2113dc
ahundiak <ahundiak@giles>
2013-12-14 12:01:34 -0600

    Few tweaks

--- src/Cerad/Bundle/AngBundle/Resources/public/js/app.js
+++ src/Cerad/Bundle/AngBundle/Resources/public/js/app.js
@@ -7,23 +7,42 @@ var ceradApp = angular.module('ceradApp', [
   'ceradServices'
 ]);
 
-ceradApp.config(['$routeProvider',
-  function($routeProvider) {
-    $routeProvider.
-      when('/games', {
-        templateUrl: 'partials/game-list.html',
-        controller: 'GameListController'
-      }).
-      when('/persons', {
-        templateUrl: 'partials/person-list.html',
-        controller: 'PersonListController'
-      }).
-      when('/person/edit/:personId', {
-        templateUrl: 'partials/person-edit.html',
-        controller: 'PersonEditController'
-      }).
-      otherwise({
-        redirectTo: '/'
-      });
-  }]);
+ceradApp.config(['$routeProvider','$locationProvider',
+    function($routeProvider,$locationProvider) 
+    {
+        /* ====================================================
+         * Latest documentation also uses forward slash for routes
+         * 
+         * controllerAs exposes the controller to the templates
+         */
+        $routeProvider.
+            when('/welcome', {
+                templateUrl: 'partials/welcome.html',
+            }).
+            when('/games', {
+                templateUrl: 'partials/game-list.html',
+                controller: 'GameListController'
+            }).
+            when('/persons', {
+                templateUrl:  'partials/person-list.html',
+                controller:   'PersonListController',
+                controllerAs: 'personListController'
+            }).
+            when('/person/edit/:personId', {
+                templateUrl: 'partials/person-edit.html',
+                controller: 'PersonEditController'
+            }).
+            otherwise({
+                redirectTo: ''
+            });
+      
+      /* ===================================================
+       * The doc has a note about needing this for jsfiddle
+       * I yhink I saw something about IE10 not working with this
+       * I think I also saw something about getting rid of # signs?
+       * 
+       * Turning this on causes the # signs to stop working?
+       */
+     //$locationProvider.html5Mode(true);    
+}]);
 
