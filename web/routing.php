a4dd53289392d1ba8f4080497d845cc111a40b42
ahundiak <ahundiak@spike.mshome.net>
2014-01-10 17:29:59 -0600

    web/routine

--- web/routing.php
+++ web/routing.php
@@ -1,7 +1,7 @@
 <?php
 // php -S localhost:8010 routing.php
 // (file_exists(__DIR__ . '/' . $_SERVER['REQUEST_URI'])) 
-
+echo $_SERVER['REQUEST_URI'];
 if (file_exists($_SERVER['REQUEST_URI'])) 
 {
     // Need a bit of a hack for /
